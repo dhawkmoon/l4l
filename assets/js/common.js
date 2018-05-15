3fe21f86b280914d32caaa7191cde10ff82e7439
Mikhail Vikrhian <m.vihrian@afonico.ru>
2018-05-15 15:43:12 +0300

    Created Section 4

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,11 +1,19 @@
 $(document).ready( function(){
-
+	//Features icons tabs
 	$('[data-toggle=tabs]').click(function(e){
 		e.preventDefault();
 		$('[data-toggle=tabs]').removeClass('active');
 		$(this).addClass('active');
-
+		var target = $(this).attr('href');
+		$('.icons-tab').removeClass('active');
+		$( target ).addClass('active')
 		return false;
 	})
+
+	//Questions accordeon
+	$('.question-item').click(function(e){
+		$('.question-item').removeClass('question-item--active')
+		$(this).addClass('question-item--active')
+	});
 
 });
