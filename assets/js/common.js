13c863531c9e2b5a997fa69b5a5bf77be42c5c9f
Mikhail Vikrhian <m.vihrian@afonico.ru>
2018-05-16 12:46:30 +0300

    Added section 8, fixed some little bug, improoved icons-tabs layout

--- assets/js/common.js
+++ assets/js/common.js
@@ -2,10 +2,10 @@ $(document).ready( function(){
 	//Features icons tabs
 	$('[data-toggle=tabs]').click(function(e){
 		e.preventDefault();
-		$('[data-toggle=tabs]').removeClass('active');
+		$(this).parents('.icons-tabs').find('[data-toggle=tabs]').removeClass('active');
 		$(this).addClass('active');
 		var target = $(this).attr('href');
-		$('.icons-tab').removeClass('active');
+		$(this).parents('.icons-tabs').find('.icons-tab').removeClass('active');
 		$( target ).addClass('active')
 		return false;
 	})
