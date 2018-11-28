586ef235f0fc5769efa5ea6272608d15c2122494
Mikhail Vikrhian <m.vihrian@afonico.ru>
2018-11-28 10:17:56 +0300

    Modified index.html, changed phones and images

--- assets/js/forms.js
+++ assets/js/forms.js
@@ -126,6 +126,7 @@
 			 var data = $form.serialize()
 			 data += '&form='+$form.attr('id')
 			 data += '&url=' + window.location.search.replace( /&/g, '-_-' )
+			 data += '&src=Симферополь'
 			 //console.log( data )
 			 $.ajax({
 					url: '/backend/send',
